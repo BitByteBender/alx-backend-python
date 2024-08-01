459d08094b0b0bc01556f1d60508f3a53328a263
Soufiane Sadgali <ssadgali2@gmail.com>
2024-08-01 14:36:21 +0100

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Basic annotations - floor """
+
+
+def floor(n: float) -> float:
+    """
+        floor func - takes 1 arg float
+        returns: float value
+    """
+    return int(n)
