017b6656664918f1b226d67b9d2e02514effe940
Soufiane Sadgali <ssadgali2@gmail.com>
2024-08-01 14:30:41 +0100

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Basic annotations - concat """
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        concat func - takes two args as a string
+        returns: concatenation of 2 provided strings
+    """
+    return str1 + str2;
