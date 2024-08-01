1cc6f17bd5b592bb1d14593a255d576cfd59b52d
Soufiane Sadgali <ssadgali2@gmail.com>
2024-08-01 14:42:45 +0100

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Basic annotations - to string """
+
+
+def to_str(n: float) -> str:
+    """
+        to_str func - takes a float arg
+        returns: a string representation
+    """
+    return str(n)
