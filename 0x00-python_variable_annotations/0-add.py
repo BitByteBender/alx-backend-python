3a6de06e7773a8791d0547a9784985f09e4e1c84
Soufiane Sadgali <ssadgali2@gmail.com>
2024-08-01 14:26:19 +0100

    Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
+""" Basic annotations - add """
 
 
 def add(a: float, b: float) -> float:
+    """
+        add func - takes 2 args as float
+        return a sum as float
+    """
     return (a + b)
