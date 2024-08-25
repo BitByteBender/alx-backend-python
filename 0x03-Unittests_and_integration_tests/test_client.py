7c309355c7501ba455147f2b6fb14b7031228cd7
Soufiane Sadgali <ssadgali2@gmail.com>
2024-08-25 20:39:57 +0100

    Mocking

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,6 @@
 """ Parameterized and patch as deco """
 
 from client import GithubOrgClient
-from fixtures import TEST_PAYLOAD
 from parameterized import parameterized, parameterized_class
 import json
 import unittest
